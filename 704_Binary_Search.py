54f75dead7afcfe30bae5ac235c8da28b92ded1c
ShoutaoSun <sunshoutao0827@163.com>
2025-03-26 11:55:07 +0800

    update

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,2 +1,33 @@
-print("Hello World")
-hello
+'''
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+'''
+from typing import List
+
+class Solution(object):
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return -1
+    
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+print(Solution.search(Solution(), nums, target))
