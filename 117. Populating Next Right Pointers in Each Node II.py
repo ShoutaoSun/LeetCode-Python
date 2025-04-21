c57b50cc75461056486329c2aeefae87b6fd5fbf
ShoutaoSun <sunshoutao0827@163.com>
2025-04-21 22:40:30 +0800

    update

---
+++ 117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,48 @@
+'''
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+
+Example:
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+'''
+
+from collections import deque
+
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return root
+        
+        queue = deque([root])
+        while queue:
+            prev = None
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                if prev:
+                    prev.next = cur
+                prev = cur
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+        return root
