b2d3dc082e785608e9fd9b0644e740a30d06f897
ShoutaoSun <sunshoutao0827@163.com>
2026-05-11 22:20:01 +0800

    update

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        dp = [[0, 0] for _ in range(len(prices))]
+        dp[0][0] = -prices[0]
         
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], -prices[i])
+            dp[i][1] = max(dp[i-1][1], prices[i] + dp[i][0])
+            
+        return dp[len(prices) - 1][1]
+        
+# test
+prices = [7, 1, 5, 3, 6, 4]
+print(Solution.maxProfit(Solution(), prices))
---
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+# test
+prices = [7, 1, 5, 3, 6, 4]
