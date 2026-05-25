dca1133f9a5c345322ba6d8337fdf20bfbd48164
Shoutao <sunshoutao0827@163.com>
2026-05-25 17:36:31 +0800

    update

---
+++ 647. Palindromic Substrings.py
@@ -0,0 +1,31 @@
+'''
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+A substring is a contiguous sequence of characters within the string.
+'''
+
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = [[False] * len(s) for _ in range(len(s))]
+        result = 0
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j]:
+                    if j - i <= 1:
+                        dp[i][j] = True
+                        result += 1
+                    elif dp[i+1][j-1]:
+                        dp[i][j] = True
+                        result += 1
+
+        return result
+        
+# test
+s = 'aaa'
+print(Solution.countSubstrings(Solution(), s))
---
+++ 72. Edit Distance.py
@@ -0,0 +1,36 @@
+'''
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+- Insert a character
+- Delete a character
+- Replace a character
+'''
+
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+        
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for j in range(len(word2) + 1):
+            dp[0][j] = j
+
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i][j-1] + 1, dp[i-1][j] + 1, dp[i-1][j-1] + 1)
+        
+        return dp[len(word1)][len(word2)] 
+
+# test
+word1 = "horse"
+word2 = "ros"
+print(Solution.minDistance(Solution(), word1, word2))
